b484ecc8ac16b87abb304186ac8c5d993ee1aa90
Helm <meyhel01@SRM-AP-00062.local>
2016-07-08 14:59:14 -0700

    Open CSV file as a list in Python Notebook

---
+++ openCSV.py
@@ -0,0 +1,6 @@
+import csv
+data = []
+with open('/Users/meyhel01/Documents/traitifydata.csv', 'rU') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        data.append(', '.join(row))
