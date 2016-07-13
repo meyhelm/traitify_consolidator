ec347ce134dc7c5249f524ac92fa4406761cce72
Helm <meyhel01@SRM-AP-00062.local>
2016-07-13 15:22:48 -0700

    k means clustering with 10 clusters

--- clustering.py
+++ clustering.py
@@ -3,11 +3,11 @@ from sklearn.cluster import KMeans
 import numpy as np
 
 
-X = pd.read_csv('/Users/meyhel01/Documents/Traitify Excel & Charts/traitifyNum.csv', sep=',', engine='c')
+X = pd.read_csv('/Users/meyhel01/Documents/Traitify Excel/traitifyNum.csv', sep=',', engine='c')
 kmeans = KMeans(n_clusters=10)
 clusters = kmeans.fit(X)
 labels = clusters.labels_
 centers = clusters.cluster_centers_
 
-np.savetxt('/Users/meyhel01/Documents/Traitify Excel & Charts/centroids.txt', centers)
-np.savetxt('/Users/meyhel01/Documents/Traitify Excel & Charts/numLabels.txt', labels)
+np.savetxt('/Users/meyhel01/Documents/Traitify Excel/centroids.txt', centers)
+np.savetxt('/Users/meyhel01/Documents/Traitify Excel/numLabels.txt', labels)
