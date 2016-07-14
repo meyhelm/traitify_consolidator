c1206a3e0d5ea7f81ffdca0d25d84df82cd4598e
Helm <meyhel01@SRM-AP-00062.local>
2016-07-14 14:57:44 -0700

    made it a function

--- blend_hist.py
+++ blend_hist.py
@@ -5,15 +5,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from collections import Counter
 
-print 'uploading data'
-my_data = pd.read_csv('/Users/meyhel01/Documents/traitifydata.csv', sep=',', engine='c')
+def histogram():
+    my_data = pd.read_csv('/Users/meyhel01/Documents/traitifydata.csv', sep=',', engine='c')
 
-personalities = my_data['Blend']
+    personalities = my_data['Blend']
+    letter_counts = Counter(personalities)
+    df = pd.DataFrame.from_dict(letter_counts, orient='index')
+    df.plot(kind='bar')
 
-letter_counts = Counter(personalities)
-df = pd.DataFrame.from_dict(letter_counts, orient='index')
-df.plot(kind='bar')
-
-ax = df.plot(kind='bar')  # s is an instance of Series
-fig = ax.get_figure()
-fig.savefig('/Users/meyhel01/Documents/histogram.pdf',bbox_inches='tight')
+    ax = df.plot(kind='bar')
+    fig = ax.get_figure()
+    fig.savefig('/Users/meyhel01/Documents/histogram.pdf',bbox_inches='tight')
